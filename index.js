f3d28fb99c1041052120ee974570b82977cfd8ac
Brasero <brandonricci4@gmail.com>
2024-05-15 14:04:13 +0200

    comment index.js

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ const server = http.createServer((req, res) => {
   //   return
   // }
   
+  
+  /*
+  * Pour gérer les redirections, il est possible de le faire grâce aux entêtes de réponse, voir ci-dessous
+  *
+  * res.writeHead(301, {
+  *   "Location": "/"
+  * })  // Le code 301 est un code HTTP qui définie que le server redirige l'utilisateur vers une autre page (301 moved permanently)
+  * // Il sera toujours accompagné du header "Location" afin de renseigner l'url vers laquelle on souhaite rediriger
+  * */
+  
+  // Lorsque l'on souhaite rendre du HTML il faut le préciser grace à l'entête "Content-Type" qui devra prendre la valeur
+  // "text/html"
   res.writeHead(200, {
     "Content-Type": 'text/html'
   })
@@ -19,9 +31,12 @@ const server = http.createServer((req, res) => {
     <html>
         <div>Hello</div>
     </html>
-  `) // Utilisez des backtick pout rendre du html (ps : pensez à mettre la bonne entête "Content-Type: text/html"
+  `) // Utilisez des backtick pout rendre du html
 })
 
+// server.listen sert à lancer l'écoute du server sur un port précis (ici le 8080)
+// la méthode liste s'attend à recevoir obligatoirement le port (8080), optionnellement le host (localhost) et une fonction
+// de backlog qui sera executé lors du démarrage du server
 server.listen(8080, 'localhost', () => {
-  console.log(`Server listening at 8080`)
+  console.log(`Server listening at http://localhost:8080`)
 })
