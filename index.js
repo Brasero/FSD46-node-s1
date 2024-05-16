392f33afaafbc563582b01fbdc9503cebf9e2cce
Brasero <brandonricci4@gmail.com>
2024-05-16 10:28:26 +0200

    preparation correction POST challenge

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 import http from 'node:http'
+import path from 'node:path'
+import fs from "node:fs";
+
+const port = 8080;
+const host = "localhost";
+
+const viewPath = path.join(import.meta.dirname, 'src', 'view') //Chemin absolu vers le dossier de vues
+const dataPath = path.join(import.meta.dirname, 'src', 'Data') //Chemin absolu vers le dossier de Data
 
 const server = http.createServer((req, res) => {
-  const url = req.url.replace('/', '')
   
-  // if (url === 'favicon.ico') {
-  //   res.writeHead(200, {
-  //     "Content-Type": "image/x-icon"
-  //   })
-  //
-  //   res.end()
-  //   return
-  // }
+  if (req.url === '/favicon.ico') {
+    res.writeHead(200, {
+      "Content-Type": "image/x-icon"
+    })
+    res.end()
+    return
+  }
   
+  //Je vérifie la route demandée
+  if(req.url === '/') {
+    //Ensuite, je vérifie la methode
+    if(req.method === 'GET') {
+      try {
+        const homePage = fs.readFileSync(path.join(viewPath, 'form.html'), {encoding: 'utf8'})
+        res.writeHead(200, {
+          "Content-Type": "text/html"
+        })
+        res.end(homePage)
+      } catch(e) {
+        res.writeHead(500, {
+          "Content-Type": "text/plain"
+        })
+        res.end(e.message)
+      }
+    }
+    
+    return
+  }
   
-  /*
-  * Pour gérer les redirections, il est possible de le faire grâce aux entêtes de réponse, voir ci-dessous
-  *
-  * res.writeHead(301, {
-  *   "Location": "/"
-  * })  // Le code 301 est un code HTTP qui définie que le server redirige l'utilisateur vers une autre page (301 moved permanently)
-  * // Il sera toujours accompagné du header "Location" afin de renseigner l'url vers laquelle on souhaite rediriger
-  * */
-  
-  // Lorsque l'on souhaite rendre du HTML il faut le préciser grace à l'entête "Content-Type" qui devra prendre la valeur
-  // "text/html"
-  res.writeHead(200, {
-    "Content-Type": 'text/html'
+  res.writeHead(404, {
+    "Content-Type" : "text/html"
   })
-  res.end(`
-    <html>
-        <div>Hello</div>
-    </html>
-  `) // Utilisez des backtick pout rendre du html
+  const page404 = fs.readFileSync(path.join(viewPath, "404.html"), {encoding: "utf8"})
+  res.end(page404)
 })
 
-// server.listen sert à lancer l'écoute du server sur un port précis (ici le 8080)
-// la méthode liste s'attend à recevoir obligatoirement le port (8080), optionnellement le host (localhost) et une fonction
-// de backlog qui sera executé lors du démarrage du server
-server.listen(8080, 'localhost', () => {
-  console.log(`Server listening at http://localhost:8080`)
+server.listen(port, host, () => {
+  console.log(`Server listening at http://${host}:${port}`)
 })
